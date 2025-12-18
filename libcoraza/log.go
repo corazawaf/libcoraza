a4b556d3c0d9839ea409f14e8c5d957a216844c9
William Zhang <wtzhang23@gmail.com>
2025-12-18 13:30:02 -0800

    undo log changes
    Signed-off-by: William Zhang <wtzhang23@gmail.com>

--- libcoraza/log.go
+++ libcoraza/log.go
@@ -3,15 +3,8 @@ package main
 /*
 typedef void (*coraza_log_cb) (const void *);
 
-typedef void (*coraza_log_closure) (void *, const char *);
-
 void send_log_to_cb(coraza_log_cb cb, const char *msg){
 	cb(msg);
-}
-
-
-void send_log_to_closure(coraza_log_closure cb, void *ctx, const char *msg){
-	cb(ctx, msg);
 }
 */
 import "C"
